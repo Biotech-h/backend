ba28649c87591e75bd1c8e460af594a971e2e680
IrinaSafenkova <105323664+IrinaSafenkova@users.noreply.github.com>
2022-06-02 10:09:12 +0300

    Feature/method for post (#4)
    * Сделан Flask endpoint для вывода вакансий
    
    * Сделан метод endpoint, обрабатывающий post запрос на добавление вакансии
    
    * Добавила в __main__ method для post по вакансиям и перенесла get по вакансиям
    
    * Исправлено на возврат добавленного списка

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,15 +1,33 @@
-from flask import Flask
+from flask import Flask, request
 import json
 
 app = Flask(__name__)
 
 companies = [{'name': 'Pfizer'},
-{'name': 'Novo Nordisk'}
+             {'name': 'Novo Nordisk'}
+             ]
+
+jobs = [{'name': 'postdoctoral researcher'}, {
+    'name': 'team leader'}, {'name': 'associate professor'}
 ]
+
 
 @app.get('/api/companies/')
 def get_all_companies():
     return json.dumps(companies)
+
+
+@app.get('/api/jobs/')
+def get_all_jobs():
+    return json.dumps(jobs)
+
+
+@app.post('/api/jobs/')
+def add_job():
+    payload = request.json
+    jobs.append(payload)
+    return payload
+
 
 if __name__ == "__main__":
     app.run(debug=True)
