2df0d9f162cdb756160ad1a949585f3d4de6e3e7
Pavel Khramov <paul.khramov@gmail.com>
2022-06-05 01:01:31 +0500

    get_company_by_id and del_company_by_id

--- backend/views/companies.py
+++ backend/views/companies.py
@@ -3,8 +3,29 @@ from flask import Blueprint
 
 routes = Blueprint('companies', __name__)
 
-companies = []
+companies = [
+    {'uid': 1, 'name': 'Moderna', 'region': 'USA', 'field': 'biotech'},
+    {'uid': 2, 'name': 'Roche', 'region': 'Switzerland', 'field': 'biotech'}
+]
+
 
 @routes.get('/')
 def get_all():
     return json.dumps(companies)
+
+
+@routes.get('/<int:id>')
+def get_company_by_id(id):
+    for company in companies:
+        if company['uid'] == id:
+            return json.dumps(company)
+    raise IndexError('Company not found')
+
+
+@routes.delete('/<int:id>')
+def del_company_by_id(id):
+    for company in companies:
+        if company['uid'] == id:
+            del companies[companies.index(company)]
+            return json.dumps(companies)
+    raise IndexError('Company not found')
