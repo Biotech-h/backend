655c6462e74e886acef2af333e8de32994ec823c
Pavel Khramov <paul.khramov@gmail.com>
2022-06-08 00:05:11 +0500

    companies_storage added

---
+++ backend/companies_storage.py
@@ -0,0 +1,29 @@
+from flask import abort
+
+
+class CompanyStorage:
+    def __init__(self):
+        self.storage = {}
+
+    def add(self, company):
+        uid = company['id']
+        self.storage[uid] = company
+
+    def delete(self, uid):
+        if not self.storage.get(uid):
+            abort(404, f'uid {uid} not found')
+        del self.storage[uid]
+
+    def update(self, company):
+        uid = company['id']
+        if not self.storage.get(uid):
+            abort(404, f'uid {uid} not found')
+        self.storage[uid] = company   
+        
+    def get_by_id(self, uid):
+        if not self.storage.get(uid):
+            abort(404, f'uid {uid} not found')
+        return self.storage[uid]
+
+    def get_all(self):
+        return self.storage
