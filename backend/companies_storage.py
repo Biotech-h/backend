0f7ed2b5bc85959c537bca6bc70cae1c6cc2f894
Pavel Khramov <paul.khramov@gmail.com>
2022-06-08 22:40:22 +0500

    companies class modified

--- backend/companies_storage.py
+++ backend/companies_storage.py
@@ -1,12 +1,13 @@
-from flask import abort, json
+from flask import abort
 
 
-class CompanyStorage:
+class CompaniesStorage:
     def __init__(self):
         self.storage = {}
 
-    def add(self, company):
-        uid = company['id']
+    def add(self, company, uid):
+        if self.storage.get(uid):
+            abort(400, f'uid {uid} already exists')
         self.storage[uid] = company
 
     def delete(self, uid):
@@ -14,8 +15,7 @@ class CompanyStorage:
             abort(404, f'uid {uid} not found')
         del self.storage[uid]
 
-    def update(self, company):
-        uid = company['id']
+    def update(self, company, uid):
         if not self.storage.get(uid):
             abort(404, f'uid {uid} not found')
         self.storage[uid] = company   
@@ -26,4 +26,4 @@ class CompanyStorage:
         return self.storage[uid]
 
     def get_all(self):
-        return json.dumps(self.storage)
+        return self.storage
--- backend/views/companies.py
+++ backend/views/companies.py
@@ -1,20 +1,25 @@
 import json
 from flask import Blueprint, abort, request
-from backend.companies_storage import CompanyStorage
+from backend.companies_storage import CompaniesStorage
 
-storage = CompanyStorage()
+storage = CompaniesStorage()
 
 routes = Blueprint('companies', __name__)
 
+@routes.post('/<int:uid>')
+def add(uid):
+    new_company = request.json
+    storage.add(new_company, uid)
+    return json.dumps(storage.get_all())
 
 @routes.get('/')
 def get_all():
-    return json.dumps(storage.get_all)
+    return json.dumps(storage.get_all())
 
 
 @routes.get('/<int:uid>')
 def get_by_id(uid):
-    company = storage.get_by_id
+    company = storage.get_by_id(uid)
     if company is None:
         abort(404, f"Unfortunately, company not found, uid: {uid}")
     return json.dumps(company)
@@ -22,13 +27,13 @@ def get_by_id(uid):
 
 @routes.delete('/<int:uid>')
 def del_by_id(uid):
-    storage.delete
-    return json.dumps(storage.get_all)
+    storage.delete(uid)
+    return json.dumps(storage.get_all())
 
 
 @routes.put('/<int:uid>')
 def change_company(uid):
     new_company = request.json
-    storage.update(new_company)
-    return json.dumps(CompanyStorage.get_all)
+    storage.update(new_company, uid)
+    return json.dumps(storage.get_all())
 
