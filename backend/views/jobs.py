5d85628269fcdfd4d87dc293a8e1035777905fd5
IrinaSafenkova <105323664+IrinaSafenkova@users.noreply.github.com>
2022-06-08 22:48:11 +0300

    Добавлены ручки на получение вакансии по id, удаление вакансии, добав… (#11)
    * Добавлены ручки на получение вакансии по id, удаление вакансии, добавление вакансии
    
    * Внесла изменения по правкам

--- backend/views/jobs.py
+++ backend/views/jobs.py
@@ -3,12 +3,37 @@ from flask import Blueprint, request
 
 routes = Blueprint('jobs', __name__)
 
-jobs = []
+jobs = {
+    1: {"title_job": "postdoctoral researcher",
+        "description": "<str:description>"},
+    2: {"title_job": "team leader",
+        "description": "<str:description>"},
+    3: {"title_job": "associate professor",
+        "description": "<str:description>"}
+}
 
 
 @routes.get('/')
 def get_all():
     return json.dumps(jobs)
+
+
+@routes.get('/<int:uid>')
+def get_by_id(uid):
+    job = jobs.get(uid)
+    return json.dumps(job)
+
+
+@routes.delete('/<int:uid>')
+def del_by_id(uid):
+    del jobs[uid]
+    return {}, 204
+
+
+@routes.put('/<int:uid>')
+def change_job(uid):
+    jobs[uid] = request.json
+    return jobs.get(uid)
 
 
 @routes.post('/')
