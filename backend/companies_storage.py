2e4a90a6fa98196ecadbf77713e55308a6674ad0
Pavel Khramov <paul.khramov@gmail.com>
2022-06-08 23:43:39 +0500

    fix: raise in companies

--- backend/companies_storage.py
+++ backend/companies_storage.py
@@ -1,5 +1,3 @@
-from flask import abort
-
 
 class CompaniesStorage:
     def __init__(self):
@@ -8,27 +6,29 @@ class CompaniesStorage:
     def add(self, company):
         uid = company['uid']
         if self.storage.get(uid):
-            abort(400, f'uid {uid} already exists')
+            raise ValueError(f'uid {uid} already exists')
 
         self.storage[uid] = company
         return company
 
     def delete(self, uid):
         if not self.storage.get(uid):
-            abort(404, f'uid {uid} not found')
+            raise ValueError(f'uid {uid} not found')
+
         del self.storage[uid]
 
     def update(self, company):
         uid = company['uid']
         if not self.storage.get(uid):
-            abort(404, f'uid {uid} not found')
+            raise ValueError(f'uid {uid} not found')
 
         self.storage[uid] = company
         return company
 
     def get_by_id(self, uid):
         if not self.storage.get(uid):
-            abort(404, f'uid {uid} not found')
+            raise ValueError(f'uid {uid} not found')
+
         return self.storage[uid]
 
     def get_all(self):
--- backend/views/companies.py
+++ backend/views/companies.py
@@ -10,7 +10,12 @@ routes = Blueprint('companies', __name__)
 @routes.post('/')
 def add():
     new_company = request.json
-    return storage.add(new_company)
+    try:
+        company = storage.add(new_company)
+    except ValueError as err:
+        abort(409, str(err))
+
+    return company
 
 
 @routes.get('/')
@@ -20,19 +25,30 @@ def get_all():
 
 @routes.get('/<int:uid>')
 def get_by_id(uid):
-    company = storage.get_by_id(uid)
-    if company is None:
-        abort(404, f"Unfortunately, company not found, uid: {uid}")
-    return json.dumps(company)
+    try:
+        company = storage.get_by_id(uid)
+    except ValueError as err:
+        abort(409, str(err))
+
+    return company
 
 
 @routes.delete('/<int:uid>')
 def del_by_id(uid):
-    storage.delete(uid)
+    try:
+        storage.delete(uid)
+    except ValueError as err:
+        abort(409, str(err))
+
     return {}, 204
 
 
 @routes.put('/<int:uid>')
 def change_company(uid):
     new_company = request.json
-    return storage.update(new_company)
+    try:
+        company = storage.update(new_company)
+    except ValueError as err:
+        abort(409, str(err))
+
+    return company
