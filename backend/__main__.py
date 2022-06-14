43573ced73e200fdb2444aa20f0cf093da005bfc
Pavel Khramov <paul.khramov@gmail.com>
2022-06-14 23:53:47 +0500

    fix: logging

--- backend/__main__.py
+++ backend/__main__.py
@@ -4,7 +4,6 @@ from backend.views import companies
 import logging
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 app.register_blueprint(jobs.routes, url_prefix='/api/v1/jobs')
