d378df9e6a01fe73b472104ac14a9ed6242938a3
Irina Safenkova <saf-iri@yandex.ru>
2022-06-15 23:58:58 +0300

    chore: добавлен универсальный обработчик ошибок

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,13 +1,19 @@
 from flask import Flask
 from backend.views import jobs
 from backend.views import companies
+from backend.errors import AppError
 import logging
 
 logging.basicConfig(level=logging.INFO)
 
+def handle_app_errors(error: AppError):
+    return {'error': error.reason}, error.status
+
+
 app = Flask(__name__)
 app.register_blueprint(jobs.routes, url_prefix='/api/v1/jobs')
 app.register_blueprint(companies.routes, url_prefix='/api/v1/companies')
+app.register_error_handler(AppError, handle_app_errors)
 
 if __name__ == "__main__":
     app.run(debug=True)
