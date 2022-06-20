de0046c03154d8bcfc2e1bc867294184a5436b4d
Pavel Khramov <paul.khramov@gmail.com>
2022-06-20 22:51:57 +0500

    fix: vacancies sqlmodel

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy import Column, Integer, String, Text, ForeignKey
 
 from backend.database.db import Base
 
@@ -16,10 +16,9 @@ class Company(Base):
 class Vacancy(Base):
     __tablename__ = 'vacancies'
 
-    company_uid = Column(Integer, nullable=False)
+    company_uid = Column(Integer, ForeignKey(Company.uid), nullable=False)
     vacancy_name = Column(String, nullable=False)
     salary = Column(Integer, nullable=False)
-    company_name = Column(String, nullable=False)
     region = Column(String, nullable=False)
     category = Column(String, nullable=False)
     description = Column(Text, nullable=False)
