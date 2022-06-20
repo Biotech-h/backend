439dff79d7f6034614926cf5863b1c068d8c4bf1
Pavel Khramov <paul.khramov@gmail.com>
2022-06-20 22:36:25 +0500

    feature: vacancies sqlmodel added

--- backend/database/models.py
+++ backend/database/models.py
@@ -11,3 +11,15 @@ class Company(Base):
     region = Column(String, nullable=False)
     category = Column(String, nullable=False)
     description = Column(Text, nullable=False)
+
+
+class Vacancy(Base):
+    __tablename__ = 'vacancies'
+
+    company_uid = Column(Integer, nullable=False)
+    vacancy_name = Column(String, nullable=False)
+    salary = Column(Integer, nullable=False)
+    company_name = Column(String, nullable=False)
+    region = Column(String, nullable=False)
+    category = Column(String, nullable=False)
+    description = Column(Text, nullable=False)
