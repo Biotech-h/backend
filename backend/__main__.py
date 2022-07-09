a2100b06385d3f0e6cc04306647bd1e7c79d0585
Pavel Khramov <paul.khramov@gmail.com>
2022-07-09 13:06:10 +0500

    feature: db_session.remove added

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,5 +1,6 @@
 import logging
 
+from database.db import db_session
 from flask import Flask
 from pydantic import ValidationError
 
@@ -18,12 +19,17 @@ def handle_validation_errors(error: ValidationError):
     return {'error': error.errors()}, 422
 
 
+def shutdown_session(exception=None):
+    db_session.remove()
+
+
 app = Flask(__name__)
 app.register_blueprint(jobs.routes, url_prefix='/api/v1/jobs')
 app.register_blueprint(companies.routes, url_prefix='/api/v1/companies')
 app.register_error_handler(AppError, handle_app_errors)
 app.register_error_handler(ValidationError, handle_validation_errors)
 
+app.teardown_appcontext(shutdown_session)
 
 if __name__ == '__main__':
     app.run(host=host, port=port)
