32d0822015f7c409daae450b120a617d9f924d92
Pavel Khramov <paul.khramov@gmail.com>
2022-07-09 15:10:37 +0500

    fix: job model

--- backend/schemas/job.py
+++ backend/schemas/job.py
@@ -10,7 +10,7 @@ class CorrectJob(BaseModel):
     name: str = Field(min_length=2)
     company_uid: int = Field(ge=1)
     salary: Optional[int]
-    description: Optional[str] = Field(max_length=100)
+    description: Optional[str] = Field(max_length=4000)
     date_published: Optional[date]
     date_expiring: Optional[date]
     url: str
